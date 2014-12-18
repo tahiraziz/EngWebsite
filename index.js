fe5d748346dd9440580b38dead93a5bcf00058bd
Tahir Aziz <tahir.aziz4@gmail.com>
2014-12-18 17:22:21 -0600

    removed tabs and made sig page

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 $(".button-collapse").sideNav();
 $('ul.tabs').tabs();
 
-/*SMOOTH SCROLLING EXCEPT ON TABS*/
+/*SMOOTH SCROLLING*/
 $(function() {
-  $('a[href*=#]:not([href=#contactAcm], [href=#event1Acm], [href=#event2Acm], [href=#aboutAcm], [href=#event1UtdHackers], [href=#event2UtdHackers], [href=#contactUtdHackers], [href=#aboutUtdHackers], [href=#event1Codeburners], [href=#event2Codeburners], [href=#contactCodeburners], [href=#aboutCodeburners], [href=#event1Wwc], [href=#event2Wwc], [href=#contactWwc], [href=#aboutWwc], [href=#event1Csg], [href=#event2Csg], [href=#contactCsg], [href=#aboutCsg], [href=#event1Lug], [href=#event2Lug], [href=#contactLug], [href=#aboutLug])').click(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
