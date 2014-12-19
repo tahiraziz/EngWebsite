a48068f7d8f338593bba6d6b3244c4c8672052b7
Tahir Aziz <tahir.aziz4@gmail.com>
2014-12-19 00:38:51 -0600

    new layout

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 $(".button-collapse").sideNav();
 $('ul.tabs').tabs();
 
-/*SMOOTH SCROLLING*/
+$('.collapsible').collapsible();
+
+/*SMOOTH SCROLLING EXCEPT ON TABS*/
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*=#]:not([href=#events], [href=#aboutAcm])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
