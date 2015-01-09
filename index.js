34116a0e502cf58c9bab85ea3f9677bd992990a7
Tahir Aziz <tahir.aziz4@gmail.com>
2015-01-08 19:58:52 -0600

    added footer, msg in contact form, announcement card, home option

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+$(document).ready(function () {
+
 $(".button-collapse").sideNav();
-$('ul.tabs').tabs();
 $('.collapsible').collapsible();
 $(".collapsible-header").click(function() {
   $(this).find('.mdi-navigation-arrow-drop-down-circle').toggleClass("rotate");
@@ -9,10 +10,9 @@ $('.input-field').find(label).mouseenter(function() {
   $(this).css("z-index", "1");
 });
 
-/*SMOOTH SCROLLING EXCEPT ON TABS*/
 $(function() {
-  $('a[href*=#]:not([href=#events], [href=#aboutAcm])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
@@ -24,3 +24,10 @@ $(function() {
     }
   });
 });
+
+
+
+
+});
+
+
